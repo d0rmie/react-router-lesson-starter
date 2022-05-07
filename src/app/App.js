230c983898b999949c55915a4d284cffb31a4ac8
dormiebasne <dormiebasne@gmail.com>
2022-05-07 14:56:20 -0400

    Update App.js
    removed unused variable

--- src/app/App.js
+++ src/app/App.js
@@ -8,7 +8,7 @@ import Article from "../components/Article";
 import Categories from "../components/Categories";
 import Author from "../components/Author";
 import Profile from "../components/Profile";
-import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import "./App.css";
 
